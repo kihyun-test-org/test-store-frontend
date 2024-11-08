87de2133a2b729615d603442453aa2d1fb18b9f5
Ubuntu <ubuntu@ip-172-31-50-115.ap-northeast-2.compute.internal>
2024-11-08 00:30:31 +0000

    first commit

---
+++ src/store-front/config/initializers/datadog.rb
@@ -0,0 +1,10 @@
+require 'datadog/statsd'
+require 'ddtrace'
+
+Datadog.configure do |c|
+  # This will activate auto-instrumentation for Rails
+  c.use :rails, {'service_name': 'store-frontend', 'cache_service': 	'store-frontend-cache', 'database_service': 'store-frontend-sqlite'}
+  # Make sure requests are also instrumented
+  c.use :http, {'service_name': 'store-frontend'}
+end
+
